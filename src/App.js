873748d6edbf1f423650c37f7de1ca698de5f142
Lenin Felix <leninfelix@hotmail.com>
2021-05-17 12:19:17 -0500

    fix init render

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ListadoNoticias from "./components/ListadoNoticias"
 function App() {
 
   // definir la categoria y noticias
-  const [categoria, guardarCategoria] = useState("");
+  const [categoria, guardarCategoria] = useState("general");
   const [noticias, guardarNoticias] = useState([])
 
   useEffect(() => {
